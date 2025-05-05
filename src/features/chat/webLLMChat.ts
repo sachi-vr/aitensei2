b5b6a2abb210cbb498aa1567fb46f72474f0c907
sachi <sachi@localhost>
2025-05-05 13:06:12 +0900

    Change LLM

--- src/features/chat/webLLMChat.ts
+++ src/features/chat/webLLMChat.ts
@@ -1,4 +1,4 @@
-import { CreateMLCEngine, MLCEngine } from "@mlc-ai/web-llm";
+import { CreateMLCEngine, MLCEngine, MLCEngineConfig, AppConfig } from "@mlc-ai/web-llm";
 import { Message } from "../messages/messages";
 
  // Initialize with a progress callback
@@ -14,7 +14,38 @@ export async function setupEngine(selectedModel: string) {
   //engine = await CreateMLCEngine("Llama-3.2-1B-Instruct-q4f32_1-MLC", { initProgressCallback });
   //engine = await CreateMLCEngine("DeepSeek-R1-Distill-Llama-8B-q4f16_1-MLC", { initProgressCallback });
   console.log("Selected model:", selectedModel);
-  engine = await CreateMLCEngine(selectedModel, { initProgressCallback });
+  //engine = await CreateMLCEngine(selectedModel, { initProgressCallback });
+  let ac: AppConfig = {
+    useIndexedDBCache: false,
+    model_list: [
+      {
+        model: "https://huggingface.co/mlc-ai/Llama-3.2-1B-Instruct-q4f32_1-MLC",
+        model_id: "Llama-3.2-1B-Instruct-q4f32_1-MLC",
+        model_lib: "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/web-llm-models/v0_2_48/Llama-3.2-1B-Instruct-q4f32_1-ctx4k_cs1k-webgpu.wasm",
+        vram_required_MB: 1128.82,
+        low_resource_required: true,
+        overrides: {
+          context_window_size: 4096,
+        },
+      },
+      {
+        model: "http://localhost:3000/dist/TinySwallow-GRPO-TMethod-experimental-q4f32_1-MLC",
+        model_id: "TinySwallow-GRPO-TMethod-experimental-q4f32_1-MLC",
+        model_lib: "http://localhost:3000/dist/TinySwallow-GRPO-TMethod-experimental-q4f32_1-webgpu.wasm",
+        //model_lib: "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/web-llm-models/v0_2_48/Llama-3.2-1B-Instruct-q4f32_1-ctx4k_cs1k-webgpu.wasm",
+        vram_required_MB: 1128.82,
+        low_resource_required: true,
+        overrides: {
+          context_window_size: 4096,
+        },
+      }
+    ]
+  };
+  let ec:MLCEngineConfig  = {
+    appConfig: ac,
+    initProgressCallback: initProgressCallback
+  };
+  engine = await CreateMLCEngine("Llama-3.2-1B-Instruct-q4f32_1-MLC",ec);
   return engine;
 }
 
